6b555c464bd9f2199da1946273c1a4a9cfb66c5c
Mary Rose Apoyon <maryroseapoyon1@gmail.com>
2018-02-10 00:40:30 +0900

    website header and footer- please replace the logo

---
+++ application/views/templates/header.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html lang="en">
+<head>
+	<title>Consumer Cooperative Solution</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Consumer Cooperative Solution">
+	<meta name="keywords" content="lab, onepage, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="assets/images/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700|Roboto:300,400,700" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="assets/css/bootstrap.css"/>
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="assets/css/flaticon.css"/>
+	<link rel="stylesheet" href="assets/css/magnific-popup.css"/>
+	<link rel="stylesheet" href="assets/css/owl.carousel.css"/>
+	<link rel="stylesheet" href="assets/css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	
+
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="logo">
+			<img src="assets/images/logo2.jpg" alt=""><!-- Logo -->
+		</div>
+		<!-- Navigation -->
+		<div class="responsive"><i class="fa fa-bars"></i></div>
+		<nav>
+			<ul class="menu-list">
+				<li class="active"><a href="<?php echo base_url(); ?>home"><i class="fa fa-home"></i>Home</a></li>
+				<li><a href="<?php echo base_url(); ?>contact"><i class="fa fa-phone"></i>Contact</a></li>
+				<li><a href="<?php echo base_url(); ?>about"><i class="fa fa-info"></i>About Us</a></li>
+				<li><a href="<?php echo base_url(); ?>"><i class="fa fa-user"></i>Log in</a></li>
+			</ul>
+		</nav>
+	</header>
+	<!-- Header section end -->
