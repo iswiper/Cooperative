eb7476aac60a2e3d503f8944c03eb7591d18bb89
Mary Rose Apoyon <maryroseapoyon1@gmail.com>
2018-02-10 21:42:15 +0900

    Please replace the logo and theme color is not final

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,7 +3,7 @@
 	<div class="hero-section">
 		<div class="hero-content">
 			<div class="hero-center">
-				<img src="assets/images/logo1.jpg" alt="">
+				<img src="assets/images/logo1.jpg" alt=""> <!---################REPLACE THIS WITH ANOTHER LOGO#######################------->
 				<div class="text-center mt60">
 					<a href="" class="site-btn">Get Started</a>
 				</div>
@@ -22,6 +22,7 @@
 
 
 		<!-- About contant -->
+		<div class="homeintro">
 		<div class="about-contant">
 			<div class="container">
 				<!-- popup video -->
@@ -30,117 +31,19 @@
 						<div class="col-md-8 col-md-offset-2">
 							<img src="assets/images/coopwhat.png" alt="">
 							<a href="https://www.youtube.com/watch?v=ju6VVM7yqSA" class="video-popup">
-								<i class="fa fa-play"></i>
+								<i class="fa-color1 fa fa-play"></i>
 							</a>
+							<div class="homeintro2">
+							<h2>What is a Consumer Cooperative Solution?</h2>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+		</div>
 		</div>
 	
 	<!-- About section end -->
 
 
-	<!-- Testimonial section -->
-	<div class="testimonial-section pb100">
-		<div class="test-overlay"></div>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8 col-md-offset-4">
-					<div class="section-title left">
-						<h2>What our clients say</h2>
-					</div>
-					<div class="owl-carousel" id="testimonial-slide">
-						<!-- single testimonial -->
-						<div class="testimonial">
-							<span>‘​‌‘​‌</span>
-							<p></p>
-							<div class="client-info">
-								<div class="avatar">
-									<img src="assets/images/noimage.png" alt="">
-								</div>
-								<div class="client-name">
-									<h2></h2>
-									<p></p>
-								</div>
-							</div>
-						</div>
-						<!-- single testimonial -->
-						<div class="testimonial">
-							<span>‘​‌‘​‌</span>
-							<p></p>
-							<div class="client-info">
-								<div class="avatar">
-									<img src="assets/images/noimage.png" alt="">
-								</div>
-								<div class="client-name">
-									<h2></h2>
-									<p></p>
-								</div>
-							</div>
-						</div>
-							<!-- single testimonial -->
-						<div class="testimonial">
-							<span>‘​‌‘​‌</span>
-							<p></p>
-							<div class="client-info">
-								<div class="avatar">
-									<img src="assets/images/noimage.png" alt="">
-								</div>
-								<div class="client-name">
-									<h2></h2>
-									<p></p>
-								</div>
-							</div>
-						</div>
-							<!-- single testimonial -->
-						<div class="testimonial">
-							<span>‘​‌‘​‌</span>
-							<p></p>
-							<div class="client-info">
-								<div class="avatar">
-									<img src="assets/images/noimage.png" alt="">
-								</div>
-								<div class="client-name">
-									<h2></h2>
-									<p></p>
-								</div>
-							</div>
-						</div>
-							<!-- single testimonial -->
-						<div class="testimonial">
-							<span>‘​‌‘​‌</span>
-							<p></p>
-							<div class="client-info">
-								<div class="avatar">
-									<img src="assets/images/noimage.png" alt="">
-								</div>
-								<div class="client-name">
-									<h2></h2>
-									<p></p>
-								</div>
-							</div>
-						</div>
-							<!-- single testimonial -->
-						<div class="testimonial">
-							<span>‘​‌‘​‌</span>
-							<p></p>
-							<div class="client-info">
-								<div class="avatar">
-									<img src="assets/images/noimage.png" alt="">
-								</div>
-								<div class="client-name">
-									<h2></h2>
-									<p></p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- Testimonial section end-->
-
-
-
+	
