84f3c793b279cb405a26c02a1a0364d82de63233
VENJIEEE <34977624+VENJIEEE@users.noreply.github.com>
2018-02-11 19:40:08 +0800

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'pages/view';
+$route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+ 
