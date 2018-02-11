8ec266296578ea011ef1978bf5f236a9a0fbffe4
VENJIEEE <34977624+VENJIEEE@users.noreply.github.com>
2018-02-11 19:53:46 +0800

    Update about.php

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,5 +1,4 @@
 
-
 	<!-- Page header -->
 	<div class="page-top-section">
 		<div class="overlay"></div>
@@ -7,8 +6,8 @@
 			<div class="page-info">
 				<h2>About</h2>
 				<div class="page-links">
-					<a href="<?php echo base_url(); ?>home">Home</a>
-					<span>About</span>
+					<a href="<?php echo base_url(); ?>home"">Home</a>
+					<span>About Us</span>
 				</div>
 			</div>
 		</div>
@@ -16,7 +15,7 @@
 	<!-- Page header end-->
 
 <!-- Team Section -->
-	<div class="team-section spad">
+	<div class="spad">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="section-title">
@@ -26,7 +25,7 @@
 				<!-- single member -->
 				<div class="col-sm-3">
 					<div class="member">
-						<img src="assets/images/noimage.png" alt="" class="img-circle">
+						<img src="assets/images/nodp.png" alt="" class="img-circle">
 						<h2>Venjie Mendez</h2>
 						<h3>Back-End Developer</h3>
 					</div>
@@ -34,7 +33,7 @@
 				<!-- single member -->
 				<div class="col-sm-3">
 					<div class="member">
-						<img src="assets/images/noimage.png" alt="" class="img-circle">
+						<img src="assets/images/nodp.png" alt="" class="img-circle">
 						<h2>Lyle Angeline Paulo</h2>
 						<h3>Back-End Developer</h3>
 					</div>
@@ -42,7 +41,7 @@
 				<!-- single member -->
 				<div class="col-sm-3">
 					<div class="member">
-						<img src="assets/images/noimage.png" alt="" class="img-circle">
+						<img src="assets/images/nodp.png" alt="" class="img-circle">
 						<h2>Edward Vincent Anabo</h2>
 						<h3>Back-End Developer</h3>
 					</div>
@@ -51,7 +50,7 @@
 				<!-- single member -->
 				<div class="col-sm-3">
 					<div class="member">
-						<img src="assets/images/noimage.png" alt="" class="img-circle">
+						<img src="assets/images/nodp.png" alt="" class="img-circle">
 						<h2>Janine Sabado</h2>
 						<h3>Front-End Developer</h3>
 					</div>
@@ -62,7 +61,7 @@
 				<!-- single member -->
 				<div class="col-sm-3">
 					<div class="member">
-						<img src="assets/images/noimage.png" alt="" class="img-circle">
+						<img src="assets/images/nodp.png" alt="" class="img-circle">
 						<h2>Mary Rose Apoyon</h2>
 						<h3>Front-End Developer</h3>
 					</div>
@@ -70,7 +69,7 @@
 				<!-- single member -->
 				<div class="col-sm-3">
 					<div class="member">
-						<img src="assets/images/noimage.png" alt="" class="img-circle">
+						<img src="assets/images/nodp.png" alt="" class="img-circle">
 						<h2>Mark Lawrence Cruz</h2>
 						<h3>Front-End Developer</h3>
 					</div>
@@ -78,7 +77,7 @@
 				<!-- single member -->
 				<div class="col-sm-3">
 					<div class="member">
-						<img src="assets/images/noimage.png" alt="" class="img-circle">
+						<img src="assets/images/nodp.png" alt="" class="img-circle">
 						<h2>Elijah Ivan Estrella</h2>
 						<h3>Quality Assurance Test</h3>
 					</div>
@@ -88,5 +87,6 @@
 					</div>
 				</div>
 			</div>
-		
+		</div>
+	</div>
 	<!-- Team Section end-->
