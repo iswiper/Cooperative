c2dabc614794d6d6db694b0a7b4f221c0935b291
MichaelLawrenceCruz <cruzolens@gmail.com>
2018-03-03 13:29:11 +0800

    bill, home and new footer

---
+++ application/views/footer.php
@@ -0,0 +1,10 @@
+</div> 
+</div>
+</div>
+<footer class="" style="clear: both;">
+	<div class="container-fluid">
+		<p><br>&copy; Copyright 2017 <strong>Cooperative</strong> | All Rights Reserverd <br> <br>  </p>
+	</div>
+</footer>
+</body>
+</html>
