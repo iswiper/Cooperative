f1c651c92f1cae8083d6fb6ff3e6f312aa98d9af
Mary Rose Apoyon <35161481+maryroseapoyon@users.noreply.github.com>
2018-03-03 19:37:28 +0800

    Add files via upload

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -48,6 +48,7 @@
           <li><a href="#about">About Us</a></li>
           <li><a href="#team">Team</a></li>
           <li><a href="#contact">Contact Us</a></li>
+		  <li><a href="#services">Services</a></li>
 		  <li><a href="#team">Signup</a></li>
 		  <li><a href="#team">Signin</a></li>
 		  
