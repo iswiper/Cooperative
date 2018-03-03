c3a9329c313eec420f779ab9e52edb26e816a1ac
VENJIEEE <venjiemendez@gmail.com>
2018-03-03 14:23:46 +0800

    additional viewsm,controls,and models

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,8 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'pages/view';
-$route['(:any)'] = 'pages/view/$1';
+
+$route['default_controller'] = 'pages/home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
- 
+$route['inventory'] = 'pages/inventory';
+$route['sales'] = 'pages/sales';
+$route['new_item'] = 'pages/new_item';
+$route['categories'] = 'pages/categories';
+$route['accounts'] = 'pages/accounts';
+$route['lagout'] = 'pages/lagout';
+$route['localhost/Cooperative'] = 'localhost/Cooperative/inventory';
+$route['pos'] = 'pos_con/pos';
+$route['daily_sales_report'] = 'sales_con/daily_sales_report';
+$route['weekly_sales_report'] = 'sales_con/weekly_sales_report';
+$route['monthly_sales_report'] = 'sales_con/monthly_sales_report';
+$route['yearly_sales_report'] = 'sales_con/yearly_sales_report';
+$route['login'] = 'login_con/home';
+$route['Home'] = 'Home_con/home';
