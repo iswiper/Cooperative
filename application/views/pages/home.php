8932bf7e11da538e38df8037d1cfd279d113bd64
Mary Rose Apoyon <35161481+maryroseapoyon@users.noreply.github.com>
2018-03-03 19:35:42 +0800

    Add files via upload

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -16,42 +16,86 @@
     ============================-->
     <section id="about">
       <div class="container">
+	  <div class="section-header">
+			 <h3 class="section-title">About Us</h2>
+            <p class="section-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+		</div>
         <div class="row about-container">
 
           <div class="col-lg-6 content order-lg-1 order-2">
-            <h2 class="title">About Us</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
 
-            <div class="icon-box wow fadeInUp">
-              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
-              <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-              <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
-            </div>
-
-            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
-              <div class="icon"><i class="fa fa-photo"></i></div>
-              <h4 class="title"><a href="">Magni Dolores</a></h4>
-              <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            </div>
-
-            <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
-              <div class="icon"><i class="fa fa-bar-chart"></i></div>
-              <h4 class="title"><a href="">Dolor Sitema</a></h4>
-              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+            <div class="img-box wow fadeInUp">
+              <div class="img"><img src="assets/img/vision.jpg" alt=""></div>
+              <h4 class="title"><a href="">Our Vision</a></h4>
+              <p class="description">We envision an economy where everyone can fulfill their needs and aspirations.</p>
             </div>
 
           </div>
 
-          <div class="col-lg-6 background order-lg-2 order-1 wow fadeInRight"></div>
+          <div class="col-lg-6 order-lg-2 order-1 wow fadeInRight">
+		  
+		   <div class="img-box wow fadeInUp">
+              <div class="img"><img src="assets/img/mission.jpg" alt=""></div>
+              <h4 class="title"><a href="">Our Mission</a></h4>
+              <p class="description">Our mission is to work with people to create cooperative businesses and networks that grow a prosperous, equitable economy.</p>
+            </div>
+		  
+		  </div>
         </div>
 
       </div>
     </section><!-- #about -->
 
-    
+    <!--==========================
+      Services Section
+    ============================--> 
+	 <section id="services">
+      <div class="container">
+		<div class="section-header">
+			 <h3 class="section-title">Services</h2>
+            <p class="section-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+		</div>
+			 <div class="row services-container">
+          <div class="col-lg-6 content order-lg-1 order-2">
+           
 
+            <div class="icon-box wow fadeInUp">
+              <div class="icon"><i class="fa fa-barcode"></i></div>
+              <h4 class="title"><a href="">POS System</a></h4>
+              <p class="description">Allows merchants to take transactions and simplify key day-to-day business operations.</p>
+            </div>
+
+            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+              <div class="icon"><i class="fa fa-bar-chart"></i></div>
+              <h4 class="title"><a href="">Sales Management</a></h4>
+              <p class="description">Records and analyze sales data to make informed business decisions.</p>
+            </div>
+
+            
+
+          </div>
+
+          <div class="col-lg-6 order-lg-2 order-1 wow fadeInRight">
+			<div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+              <div class="icon"><i class="fa fa-calendar"></i></div>
+              <h4 class="title"><a href="">Inventory Management</a></h4>
+              <p class="description">Manage quantities of stocks to determine when and how often to reorder products.</p>
+            </div>
+			
+			<div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+              <div class="icon"><i class="fa fa-credit-card"></i></div>
+              <h4 class="title"><a href="">Cooperative Advantage Card</a></h4>
+              <p class="description">Registered members of the cooperative are given points to collect every purchase.</p>
+            </div>
+		  </div>
+        </div>
+
+      </div>
+    </section>
     
 
     <!--==========================
@@ -80,7 +124,7 @@
 
           <div class="col-lg-3 col-md-6">
             <div class="member">
-              <div class="pic"><img src="assets/img/l.jpg" alt=""></div>
+              <div class="pic"><img src="assets/img/lyle.jpg" alt=""></div>
               <h4>Lyle Angeline Paulo</h4>
               <span>Back-end Developer</span>
               <div class="social">
@@ -92,9 +136,24 @@
             </div>
           </div>
 
+		   <div class="col-lg-3 col-md-6">
+            <div class="member">
+              <div class="pic"><img src="assets/img/edward.jpg" alt=""></div>
+              <h4>Edward Vincent Anabo</h4>
+              <span>Back-end Developer</span>
+              <div class="social">
+                <a href=""><i class="fa fa-twitter"></i></a>
+                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href=""><i class="fa fa-google-plus"></i></a>
+                <a href=""><i class="fa fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+		  
+		  
           <div class="col-lg-3 col-md-6">
             <div class="member">
-              <div class="pic"><img src="assets/img/r.jpg" alt=""></div>
+              <div class="pic"><img src="assets/img/maryrose.jpg" alt=""></div>
               <h4>Mary Rose Apoyon</h4>
               <span>Front-end Developer</span>
               <div class="social">
@@ -106,23 +165,10 @@
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-6">
-            <div class="member">
-              <div class="pic"><img src="assets/img/ed.jpg" alt=""></div>
-              <h4>Edward Vincent Anabo</h4>
-              <span>Back-end Developer</span>
-              <div class="social">
-                <a href=""><i class="fa fa-twitter"></i></a>
-                <a href=""><i class="fa fa-facebook"></i></a>
-                <a href=""><i class="fa fa-google-plus"></i></a>
-                <a href=""><i class="fa fa-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
 			
 			<div class="col-lg-3 col-md-6">
             <div class="member">
-              <div class="pic"><img src="assets/img/j.jpg" alt=""></div>
+              <div class="pic"><img src="assets/img/janine.jpg" alt=""></div>
               <h4>Janine Sabado</h4>
               <span>Front-end Developer</span>
               <div class="social">
@@ -136,7 +182,7 @@
 		  
 		  <div class="col-lg-3 col-md-6">
             <div class="member">
-              <div class="pic"><img src="assets/img/m.jpg" alt=""></div>
+              <div class="pic"><img src="assets/img/lawrence.jpg" alt=""></div>
               <h4>Michael Lawrence Cruz</h4>
               <span>Front-end Developer</span>
               <div class="social">
