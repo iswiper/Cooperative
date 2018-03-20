2262b813affbc58647178abb19b3a181d72cd52e
darkwillow02 <edwardvincent17@gmail.com>
2018-03-21 02:01:43 +0800

    updated db

--- db/cooperative.sql
+++ db/cooperative.sql
@@ -3,12 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2018 at 02:33 PM
+-- Generation Time: Mar 20, 2018 at 06:05 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
--- Generation Time: Mar 20, 2018 at 02:02 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -45,11 +42,8 @@ CREATE TABLE `accounts` (
 --
 
 INSERT INTO `accounts` (`id`, `username`, `password`, `account_type`, `date_created`, `created_by`) VALUES
-(12, 'Cooperative', '$2y$10$z6FEETw1gx0AVj.4kJOWXeDQs/RRDJf1pWEFJr3C9UUeAXek7mlIm', 'Admin', '2018-03-09 08:39:37 am', 'Cooperative Company');
-INSERT INTO `accounts` (`id`, `username`, `password`, `date_created`, `created_by`) VALUES
-(12, 'Cooperative', '$2y$10$z6FEETw1gx0AVj.4kJOWXeDQs/RRDJf1pWEFJr3C9UUeAXek7mlIm', '2018-03-09 08:39:37 am', 'Cooperative Company'),
-(13, 'Cooperative2', '$2y$10$bR.DHdHSKpm8V7neKNtKqOw38oi2L91vbnT5rQKzD02DUMwciC.Nm', '2018-03-17 11:39:41 am', 'Cooperative Company'),
-(14, 'Cooperative3', '$2y$10$jzVRCy/gmWcyP3JB7TUZ3OzdwfZskOa.p/S4nXyaoK9Lkj3KtduJq', '2018-03-20 07:56:21 am', 'Cooperative Company');
+(12, 'Cooperative', '$2y$10$z6FEETw1gx0AVj.4kJOWXeDQs/RRDJf1pWEFJr3C9UUeAXek7mlIm', 'Admin', '2018-03-09 08:39:37 am', 'Cooperative Company'),
+(13, 'hello', '$2y$10$RPRk06q1dJ.yrwFtLoAPge2KbBeKuNqSix7N0lCRTB/u7vEqVx4uu', '', '2018-03-20 11:26:06 pm', 'Cooperative Company');
 
 -- --------------------------------------------------------
 
@@ -72,7 +66,8 @@ INSERT INTO `category` (`id`, `date_time`, `category`, `creator`) VALUES
 (2, '2018-03-09 10:21:12 am', 'Gel', 'Cooperative Company'),
 (3, '2018-03-15 11:11:09 pm', 'Mango', 'Cooperative Company'),
 (4, '2018-03-18 09:04:58 pm', 'apple', 'Cooperative Company'),
-(5, '2018-03-19 01:05:10 am', 'melon', 'Cooperative Company');
+(5, '2018-03-19 01:05:10 am', 'melon', 'Cooperative Company'),
+(6, '2018-03-20 09:45:22 pm', 'stress reliever', 'Cooperative Company');
 
 -- --------------------------------------------------------
 
@@ -96,8 +91,6 @@ CREATE TABLE `customer` (
 -- Dumping data for table `customer`
 --
 
-
-
 INSERT INTO `customer` (`id`, `first_name`, `last_name`, `email`, `phone`, `address`, `username`, `password`, `date_created`) VALUES
 (1, 'qwertyuiop', 'sqqqqqqqqq', 'q@gmail.com', 2147483647, 'sdjsdnklsckcksc', 'qwertyuiop', '$2y$10$niEp9b23Gj1Q86P.AXA7ruY5kOeXYf.AfmDHIii/8SMl9oZGPJIzC', '2018-03-14 06:21:11 pm'),
 (2, 'wwwwwwwwwwww', 'wwwwwwwwwwww', 'w@gmail.com', 2147483647, 'wwwwwwwwwwwwwwww', 'qwertyuiop', '$2y$10$o0bKHU1wXLn8qU.H/LCOYOa1XV4UWed8KY1FDoOluAyp3ilBCm4JK', '2018-03-14 06:26:22 pm'),
@@ -109,9 +102,6 @@ INSERT INTO `customer` (`id`, `first_name`, `last_name`, `email`, `phone`, `addr
 (8, 'DSDSSDS', 'dddddd', 'q@gmail.com', 2147483647, 'dddddddddddddddddddddddddddd', 'master ', '$2y$10$xRSZ9BE0o2cL01ajSGoMXuvhhy4dbfiZk9Es1ZZNIZ.hrE3C6m9P2', '2018-03-14 07:34:47 pm'),
 (9, 'lian Nicoli', 'Paulo', 'lian@gmail.com', 2147483647, 'huakajkjahaha', 'lanpla', '$2y$10$0gi2EsBUNkr0EUOUMOCi0eOPc0fgZC6aSYVIvoQnHWNcJi8NPLCfS', '2018-03-15 11:23:33 pm'),
 (10, 'Nimely', 'Acuna', 'acunanimely@yahoo.com', 2147483647, '3738 dahuihi paru', 'acuna123', '$2y$10$jtkyclOxU35Z/C2ALh2fHufl5aT3FhCx/1aj2VZslbHSogFjRmbWW', '2018-03-16 08:18:32 am');
-
-INSERT INTO `customer` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `date_created`) VALUES
-(1, 'Venjie', 'Mendez', 'bajak@gmail.com', 'binje', '$2y$10$aQbxvuGWXWJikeqOUFtU5uFGw5q4WJzUuQHjRX7D8NreX14eiQ9Km', '2018-03-20 08:07:08 am');
 
 -- --------------------------------------------------------
 
@@ -136,9 +126,8 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`id`, `name`, `category`, `description`, `date_time`, `creator`, `quantities`, `price`, `status`) VALUES
-(2, 'Titanic Gel', 'melon', '9g hardens all of your hair on the head\r\n', '2018-03-09 10:21:46 am', 'Cooperative Company', 810, 12, '23');
-(1, 'Titanic Gel', 'Gel', '9g Hardens Your Hair', '2018-03-20 08:46:34 pm', 'Cooperative Company', 891, 90, ''),
-(2, 'Titanicgate', 'toothpaste', '1g Hardens Your teeth', '2018-03-20 08:50:55 pm', 'Cooperative Company', 0, 90, '');
+(4, 'Mangoooo', 'Mango', 'fruit fruit fruit', '2018-03-21 12:36:57 am', 'Cooperative Company', 0, 0, ''),
+(5, 'ertyuio', 'Gel', 'YUUIOP', '2018-03-21 12:38:50 am', 'Cooperative Company', 0, 0, '');
 
 -- --------------------------------------------------------
 
@@ -188,8 +177,8 @@ CREATE TABLE `supplier` (
 --
 
 INSERT INTO `supplier` (`id`, `supplier_name`, `product`, `supplier_location`, `contact`, `date_time`) VALUES
-(1, 'qwertyuiop', 'qwertyuiop', 'qwertyuiop', 123456789, 2018);
-(1, 1089775421, '2018-03-20 20:47:45', '1', 'Titanic Gel', 90, 9, 810, '2018-03-20', '03', '2018', '12');
+(1, 'qwertyuiop', 'qwertyuiop', 'qwertyuiop', 123456789, 2018),
+(2, 'qwertyuiopoiuytrew', 'qwertyuiop', 'qwertyuiuhtrew', 2147483647, 2018);
 
 --
 -- Indexes for dumped tables
@@ -239,27 +228,25 @@ ALTER TABLE `supplier`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `sales`
@@ -271,8 +258,7 @@ ALTER TABLE `sales`
 -- AUTO_INCREMENT for table `supplier`
 --
 ALTER TABLE `supplier`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
